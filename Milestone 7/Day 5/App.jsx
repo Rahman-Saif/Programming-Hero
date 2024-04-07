377dd48bd78bd842e300afbf72b74c5dcb58664a
Saif Rahman <rahmansaif.413@gmail.com>
2024-04-07 14:56:08 +0600

    Add files via upload

---
+++ Milestone 7/Day 5/App.jsx
@@ -0,0 +1,15 @@
+import Countries from './components/Countries.jsx/Countries'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>React World Tour!</h1>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
