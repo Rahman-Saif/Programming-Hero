aad729cb0df3b97f88b84d2055f42a2015c1ae42
Saif Rahman <rahmansaif.413@gmail.com>
2024-04-10 02:46:07 +0600

    Add files via upload

---
+++ Milestone 7/Day6-practice/App.jsx
@@ -0,0 +1,18 @@
+
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import Bottles from './components/bottle/Bottles'
+import Bottle from './components/bottle/Bottle';
+
+function App() {
+  
+
+  return (
+    <>
+      <Bottles/>
+    </>
+  )
+}
+
+export default App
