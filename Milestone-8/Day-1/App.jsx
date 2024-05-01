5a6e42cac1783c4e32568c3f75c7f14a2fe2950f
Saif Rahman <rahmansaif.413@gmail.com>
2024-05-01 11:53:53 +0600

    Add files via upload

---
+++ Milestone-8/Day-1/App.jsx
@@ -0,0 +1,36 @@
+
+import './App.css'
+import Nav from './components/Nav/Nab/Nav'
+import Navbar from './components/Nav/Navbar'
+import LineChart from './components/linechart/LineChart'
+import PriceOptions from './components/priceoptions/PriceOptions'
+
+function App() {
+  
+
+  return (
+    <>
+      <Nav></Nav>
+      <PriceOptions></PriceOptions>
+      <LineChart></LineChart>
+    </>
+  )
+}
+
+export default App
+
+/*
+step 1 : tailwind,daisy ui setup disi
+step 2 : Nav banailam
+step 3 : nijera alada fake data create kore tarpor oita map kore nav banailam
+step 4 : toggle button banailam state use kore - click e state set hobe, ekek state e ekek emoji show korbe 
+
+
+
+
+
+
+
+
+
+*/
